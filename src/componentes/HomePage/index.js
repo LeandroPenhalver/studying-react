f356d738b0678005a9f39516acd496dbc0c04248
LeandroPenhalver <38468347+LeandroPenhalver@users.noreply.github.com>
2020-08-22 16:23:35 -0300

    Create index.js

---
+++ src/componentes/HomePage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from '../Card/';
+
+export default function HomePage() {
+  return(
+    <div>
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
