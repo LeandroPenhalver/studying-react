29cafc31fdeada239321907e5aa0d74f77caefc2
LeandroPenhalver <38468347+LeandroPenhalver@users.noreply.github.com>
2020-08-22 16:25:50 -0300

    Update index.js

--- src/componentes/Card/index.js
+++ src/componentes/Card/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function() {
+export default function Card() {
   return(
    <div>
      <p>Componentes:</p>
      <p>Facilita em manter partes menores funcionando corretamente</p>
      <p>Você pode reutilizá-los, ou seja, menos código para se escrever</p>
-  </div>
+  </div> 
   );
 }
