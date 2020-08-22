dfe3fd2ca1fda0ffb6dade3cd14ce143971c30e3
LeandroPenhalver <38468347+LeandroPenhalver@users.noreply.github.com>
2020-08-22 16:13:13 -0300

    Create index.js

---
+++ src/componentes/Card/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function() {
+  return(
+   <div>
+     <p>Componentes:</p>
+     <p>Facilita em manter partes menores funcionando corretamente</p>
+     <p>Você pode reutilizá-los, ou seja, menos código para se escrever</p>
+  </div>
+  );
+}
