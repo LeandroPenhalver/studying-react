01dbf2809fff3416bbb0fb64908fa3047613b070
LeandroPenhalver <38468347+LeandroPenhalver@users.noreply.github.com>
2020-08-22 22:00:33 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Header from './componentes/Header';
+import HomePage from './componentes/HomePage';
 import Footer from './componentes/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HomePage />
       <Footer />
     </div>
   );
